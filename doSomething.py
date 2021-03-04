c8b69465ce6583772b37a2be8a512d9e0e6c86de
Kenny <kez32@pitt.edu>
2021-03-03 19:27:34 -0500

    refining update

--- doSomething.py
+++ doSomething.py
@@ -1,3 +1,3 @@
 name = input("What is your name?")
 weather = input("Today's weather is: ")
-print("Hello " + name + ".The weather today is: " + weather)
+print("Hello " + name + ".The weather today is " + weather + " outside.")
